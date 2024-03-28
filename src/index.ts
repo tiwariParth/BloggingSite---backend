04d3a5d0b88bf29c9da257c5de00ff43f898f501
tiwariParth <243mjparth@gmail.com>
2024-03-28 09:14:32 +0530

    feat: :sparkles: Added JWT token
    Update index.ts to include JWT token in signup response

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
-import { decode, sign, verify } from 'hono/jwt'
+import { decode, sign, verify } from "hono/jwt";
 
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string;
+    JWT_SECRET: string;
   };
 }>();
 
@@ -15,19 +16,30 @@ app.post("/api/v1/user/signup", async (c) => {
     datasourceUrl: c.env?.DATABASE_URL,
   }).$extends(withAccelerate());
   try {
-    await prisma.user.create({
+    const user = await prisma.user.create({
       data: {
         email: body.email,
         password: body.password,
         name: body.name,
       },
     });
+    const JWT = await sign(
+      {
+        id: user.id,
+      },
+      c.env?.JWT_SECRET
+    );
     c.status(201);
-    return c.text("Signup successful!");
+    return c.json({
+      message: "Signup successful!",
+      token: JWT,
+    });
   } catch (error) {
     c.status(400);
     console.log(error);
-    return c.text("Signup failed!");
+    return c.json({
+      error: "Signup failed!",
+    });
   }
 });
 app.post("/api/v1/signin", (c) => {
