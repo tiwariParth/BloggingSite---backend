a9442fc8cf9100f3eb456272907ad5444c774a21
tiwariParth <243mjparth@gmail.com>
2024-03-28 10:35:42 +0530

    Add CRUD operations for blog posts

--- src/routes/blogRouter.ts
+++ src/routes/blogRouter.ts
@@ -10,18 +10,114 @@ const blogRouter = new Hono<{
   };
 }>();
 
-blogRouter.get("/:id", (c) => {
-  const id = c.req.param("id");
-  console.log(id);
-  return c.text("get blog route");
+blogRouter.post("/", async (c) => {
+  const body = await c.req.json();
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const blog = await prisma.post.create({
+      data: {
+        title: body.title,
+        content: body.content,
+        authorId: 1,
+      },
+    });
+
+    c.status(201);
+    return c.json({
+      message: "Blog created successfully!",
+      blog: blog.id,
+    });
+  } catch (error) {
+    c.status(400);
+    console.log(error);
+    return c.json({
+      error: "Blog creation failed!",
+    });
+  }
 });
 
-blogRouter.post("/", (c) => {
-  return c.text("signin route");
+blogRouter.put("/", async (c) => {
+  const body = await c.req.json();
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const blog = await prisma.post.update({
+      where: {
+        id: body.id,
+      },
+      data: {
+        title: body.title,
+        content: body.content,
+      },
+    });
+
+    c.status(201);
+    return c.json({
+      message: "Blog updated successfully!",
+      blog: blog.id,
+    });
+  } catch (error) {
+    c.status(400);
+    console.log(error);
+    return c.json({
+      error: "Blog updation failed!",
+    });
+  }
 });
 
-blogRouter.put("/", (c) => {
-  return c.text("signin route");
+blogRouter.get("/:id", async (c) => {
+  const body = await c.req.json();
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const blog = await prisma.post.findFirst({
+      where: {
+        id: body.id,
+      },
+    });
+
+    c.status(201);
+    return c.json({
+      message: "Blog found",
+      blog: blog,
+    });
+  } catch (error) {
+    c.status(400);
+    console.log(error);
+    return c.json({
+      error: "Blog not found",
+    });
+  }
 });
+
+blogRouter.get("/", async (c) => {
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env?.DATABASE_URL,
+    }).$extends(withAccelerate());
+  
+    try {
+     const blog = await prisma.post.findMany();
+  
+      c.status(201);
+      return c.json({
+        message: "Blog found",
+        blog: blog,
+      });
+    } catch (error) {
+      c.status(400);
+      console.log(error);
+      return c.json({
+        error: "Blog not found",
+      });
+    }
+  });
+
 
 export default blogRouter;
