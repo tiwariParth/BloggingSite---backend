79633859d4de37d929daeeda1f708f206911eadd
tiwariParth <243mjparth@gmail.com>
2024-03-31 19:00:13 +0530

    feat: :sparkles: Cors intialization
    CORS is now initialize and production backend is updated

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import { Hono } from "hono";
-import { PrismaClient } from "@prisma/client/edge";
-import { withAccelerate } from "@prisma/extension-accelerate";
-import { decode, sign, verify } from "hono/jwt";
 import userRouter from "./routes/userRouter";
 import blogRouter from "./routes/blogRouter";
+import { cors } from "hono/cors";
 
 const app = new Hono<{
   Bindings: {
@@ -11,7 +9,7 @@ const app = new Hono<{
     JWT_SECRET: string;
   };
 }>();
-
+app.use("/*", cors());
 app.route("/api/v1/user", userRouter);
 app.route("/api/v1/blog", blogRouter);
 
